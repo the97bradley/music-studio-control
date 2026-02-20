bdcde4db239b58830106a4b3267f97ffaf3c2af6
the97bradley <the97bradley@gmail.com>
2026-02-20 12:47:46 -0700

    hardening gitignore and startup

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ ! -d "$VENV" ]; then
+  python3 -m venv "$VENV"
+  source "$VENV/bin/activate"
+  pip install -r requirements.txt
+else
+  source "$VENV/bin/activate"
+fi
+
 source osc-env/bin/activate
 
 # Required env
